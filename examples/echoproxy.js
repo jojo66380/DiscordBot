74034c2252b0a85684e4ee47332f0ea427bd367e
jojo66380 <joffrey.pijoan@imerir.com>
2017-04-12 09:29:14 +0200

    Add files via upload

--- examples/echoproxy.js
+++ examples/echoproxy.js
@@ -2,12 +2,13 @@
 
 // voice echo bot
 
-// records and sends audio using `proxy` mode (without decoding/encoding/saving anything)
-// variable `var recordTime = 10;` controls duration of recording in seconds
+// simple audio proxy - mirrors user's audio
+//   can be useful for redirecting audio into another channel/server through
+//   different bot instance
 
 // commands:
-// ~!~echo - joins voice channel and records audio, then plays recorded audio back
-// ~!~stop - stops recording/playing
+// ~!~start - starts proxying audio for the invoker
+// ~!~stop - stops proxying audio for the invoker
 
 var fs = require('fs');
 
@@ -22,13 +23,7 @@ try { auth = require("./auth"); } catch(e) {}
 
 client.connect(auth);
 
-var recordTime = 10;
-var recordTimer = null;
-
-var recordReply = null;
-var recordStartTime = null;
-var recordedData = [];
-var recordingUser = null;
+var proxyingUser = null;
 
 client.Dispatcher.on("MESSAGE_CREATE", e => {
   console.log("new message: ");
@@ -38,94 +33,50 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
   const content = e.message.content;
   const guild = e.message.channel.guild;
 
-  if (content == "~!~echo") {
-    if (recordingUser)
-      return e.message.reply("Already recording");
+  if (content == "~!~start") {
+    if (proxyingUser)
+      return e.message.reply("Already proxying");
 
     const channelToJoin = e.message.author.getVoiceChannel(guild);
     if (!channelToJoin)
       return e.message.reply("Join a voice channel first");
 
     channelToJoin.join().then(info => {
-      recordReply = e.message.reply.bind(e.message);
-      e.message.reply("Recording audio for " + recordTime + " seconds");
-      recordedData = [];
-      recordingUser = e.message.author.id;
-      recordStartTime = Date.now();
-
-      recordTimer = setTimeout(() => {
-        play(info);
-        e.message.reply("Playing");
-      }, recordTime * 1000);
+      e.message.reply("Proxying audio for user " + e.message.author.username);
+      proxyingUser = e.message.author.id;
     });
   }
 
   if (content == "~!~stop") {
     e.message.reply("Stopped");
-    stopPlaying = true;
-    recordingUser = false;
-    if (recordTimer) {
-      clearTimeout(recordTimer);
-      recordTimer = null;
-    }
+    proxyingUser = null;
   }
 });
 
 client.Dispatcher.on("VOICE_CONNECTED", e => {
-  e.voiceConnection.getDecoder()
-  .onPacket = (packet) => {
+  var encoder = e.voiceConnection.getEncoder({ frameDuration: 20, proxy: true });
+  var decoder = e.voiceConnection.getDecoder();
+
+  decoder.onPacket = (packet) => {
+    if (!proxyingUser) return;
+
     const user = e.voiceConnection.ssrcToMember(packet.ssrc);
     if (!user) return;
-    if (user.id != recordingUser) return;
-    packet.playbackTime = Date.now() - recordStartTime;
-    recordedData.push(packet);
+    if (user.id != proxyingUser) return;
 
     const name = user ? user.username : "<unknown>";
     console.log(
-      "recording " + packet.chunk.length +
+      "proxying " + packet.chunk.length +
       " bytes from " + name +
       " @ " + packet.timestamp
     );
+
+    var numSamples = opus_packet_get_samples_per_frame(packet.chunk);
+    encoder.enqueue(packet.chunk, numSamples);
   };
   // set callback on `.onPacketDecoded` to enable decoding and
   // have decoded audio in `packet.chunk`
 });
-
-var stopPlaying = false;
-function play(info) {
-  stopPlaying = false;
-  var playbackStartTime = Date.now();
-
-  if (!client.VoiceConnections.length)
-    return console.log("Voice not connected");
-
-  if (!info) info = client.VoiceConnections[0];
-  var voiceConnection = info.voiceConnection;
-
-  var encoder = voiceConnection.getEncoder({ frameDuration: 20, proxy: true });
-
-  function sendPacket() {
-    var packet = recordedData[0];
-    if (!packet && recordReply) {
-      recordReply("Finished playing");
-    }
-    if (!packet || stopPlaying) {
-      recordingUser = null;
-      return;
-    }
-    var currentTime = (Date.now() - playbackStartTime);
-    var nextTime = packet.playbackTime - currentTime;
-    setTimeout(sendPacket, nextTime);
-
-    if (currentTime < nextTime) return;
-
-    recordedData.shift(packet);
-    var numSamples = opus_packet_get_samples_per_frame(packet.chunk);
-    console.log("playing ", packet.chunk.length, numSamples);
-    encoder.enqueue(packet.chunk, numSamples);
-  }
-  sendPacket();
-}
 
 client.Dispatcher.onAny((type, args) => {
   console.log("\nevent "+type);
